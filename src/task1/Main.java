2112b22226e63455ac5c313b4af6d2dca7d06cb3
Yuliya Shcherbakova <yula.novosib@gmail.com>
2023-12-15 00:33:38 +0700

    Finished MyArrayList.java

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,12 +1,27 @@
 package task1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
+        MyArrayList<Integer> myArr = new MyArrayList<>();
+        myArr.add(5);
+        myArr.add(7);
+        myArr.add(15);
+        myArr.add(1);
+        myArr.add(4);
 
-        int[] arr = { 6, 4, 2, 9, 10, 15, 78, 1};
-        System.out.println(Arrays.toString(BubbleSort.bubbleSort(arr)));
+        ArrayList<Integer> collection = new ArrayList<>();
+        for (int i = 10; i >= 1; i--) {
+            collection.add(i);
+        }
+        System.out.println(myArr);
+        myArr.remove(3);
+        myArr.set(0, 50);
+        System.out.println(myArr);
+        myArr.addAll(collection);
+        System.out.println(myArr);
     }
 }
--- src/task1/MyArrayList.java
+++ src/task1/MyArrayList.java
@@ -1,19 +1,79 @@
 package task1;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class MyArrayList<T> {
 
     /*    Необходимо написать свою реализацию коллекции на выбор LinkedList или ArrayList(можно оба варианта).
      Должны быть основные методы add, get, remove, addAll, остальное на ваше усмотрение
      */
 
-    private static final int DEFAULT_CAPACITY = 10;
+    private T[] arrList;                                //тут будем хранить все элементы коллекции c любым типом данных
+    private int size;                                    //счетчик действительно хранящихся в массиве элементов
+    private static final int DEFAULT_CAPACITY = 10;      //размер ArrayList по умолчанию
 
-    transient Object[] elementData;         //тут будем хранить все элементы коллекции
-
-    private int size;       //счетчик действительно хранящихся в массиве элементов
-
+    private int capacity;
     public MyArrayList() {
+        this.capacity = DEFAULT_CAPACITY;
+        this.size = 0;
+        arrList = (T[]) new Object[capacity];
+    }
+
+    public MyArrayList(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        arrList = (T[]) new Object[capacity];
+    }
+
+    public T get(int index){
+        return arrList[index];
+    }
+
+    public void set(int index, T item){
+        arrList[index] = item;
+    }
+    public void add(T newElem){
+        if (size + 1 > capacity) {
+            T[] newArr =  (T[]) new Object[capacity * 2];
+            System.arraycopy(arrList, 0, newArr, 0, arrList.length);
+            arrList = newArr;
+        }
+        arrList[size++] = newElem;
+    }
+
+    public void remove(int index){
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        for(int i = index; i < size; i++){
+            arrList[i] = arrList[i+1];
+        }
+        size--;
+    }
+
+    public boolean addAll(Collection<? extends T> collections) {
+        for (T el : collections) {
+            add(el);
+        }
+        return true;
+    }
+
+    public int size () {
+        return size;
+    }
+
+    public void printArrList(){
+        for(T el : arrList){
+            System.out.println(el + " ");
+        }
     }
 
 
+    @Override
+    public String toString() {
+        return Arrays.toString(arrList) +
+                ", size=" + size +
+                '}';
+    }
 }
